936bdccd2d814ce4c5638acff72166c077ee2857
Ekaterina <ya.ekaterina.g@gmail.com>
2023-11-17 15:19:12 +0300

    Added transitions between pages

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 	
 	def should_be_login_url(self):
 		# проверка на корректный url адрес
-		assert 'login' in self.ccurrent_url, "url is not contain text LOGIN"
+		assert 'login' in self.browser.current_url, "url is not contain text LOGIN"
 	
 	def should_be_login_form(self):
 		# проверка, что есть форма логина
--- test_main_page.py
+++ test_main_page.py
@@ -12,4 +12,6 @@ def test_guest_can_go_to_login_page(browser):
 	page = MainPage(browser, link)
 	page.open()
 	page.go_to_login_page()			# выполняем метод страницы — переходим на страницу логина
+	login_page = LoginPage(browser, browser.current_url)
+	login_page.should_be_login_page()
 	
