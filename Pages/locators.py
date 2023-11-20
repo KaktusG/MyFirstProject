df29d3f2999467dcdec90dc4a98bda7d256b8f23
Ekaterina <ya.ekaterina.g@gmail.com>
2023-11-20 14:35:38 +0300

    Added new test - checking checking adding to basket

--- Pages/locators.py
+++ Pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
 	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
 	REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+	BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+	ITEM_NAME = (By.XPATH, "//div[@id='content_inner']/article/div/div[2]/h1")
+	ITEM_PRICE = (By.XPATH, "//div[@id='content_inner']/article/div/div[2]/p")
+	MESSAGE_BLOCK_WITH_TXT = (By.XPATH, "//div[@id='messages']/div[1]/div/strong")
+	MESSAGE_BLOCK_WITH_PRICE = (By.XPATH, "//div[@id='messages']/div[3]/div/p/strong")
---
+++ Pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+	def add_to_basket(self):
+		btn_add_to_basket = self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
+		btn_add_to_basket.click()
+		
+	def item_name (self):
+		return self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+		
+	def item_price (self):
+		return self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+
+	def should_be_text_message_with_item_name(self): 
+		message_text = self.browser.find_element(*ProductPageLocators.MESSAGE_BLOCK_WITH_TXT).text
+		item_name = self.item_name()
+		assert message_text == item_name, " The text doesn't match"
+		
+	def should_be_text_message_with_price(self):
+		message_price = self.browser.find_element(*ProductPageLocators.MESSAGE_BLOCK_WITH_PRICE).text
+		item_price = self.item_price()
+		assert message_price == item_price, "The price doesn't match"
